9bc8fe1a418096d4e2e25b838ca5890f3d9629fa
Alfredo Delgado <alfred@adelgado.org>
2017-04-24 10:26:27 -0400

    add authentication

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,7 @@
 const stores = require('./stores/stores.service.js');
+const users = require('./users/users.service.js');
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
   app.configure(stores);
+  app.configure(users);
 };
