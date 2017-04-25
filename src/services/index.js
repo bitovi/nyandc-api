4dda9370855a4b3df9f282585f1076aefd3d2d66
Alfredo Delgado <alfred@adelgado.org>
2017-04-24 23:14:09 -0400

    Added Inventory service.

--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,11 @@
 const stores = require('./stores/stores.service.js');
 const users = require('./users/users.service.js');
 const products = require('./products/products.service.js');
+const inventory = require('./inventory/inventory.service.js');
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
   app.configure(stores);
   app.configure(users);
   app.configure(products);
+  app.configure(inventory);
 };
