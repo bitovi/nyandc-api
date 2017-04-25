c6394541c8141dde23486e10cc258fd4c2cf0384
Alfredo Delgado <alfred@adelgado.org>
2017-04-24 21:16:03 -0400

    Add products service.

---
+++ src/models/products.model.js
@@ -0,0 +1,16 @@
+// products-model.js - A mongoose model
+//
+// See http://mongoosejs.com/docs/models.html
+// for more of what you can do here.
+module.exports = function (app) {
+  const mongooseClient = app.get('mongooseClient');
+  const products = new mongooseClient.Schema({
+    baseProductId: { type: String, required: true },
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now }
+  });
+
+  return mongooseClient.model('products', products);
+};
--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,9 @@
 const stores = require('./stores/stores.service.js');
 const users = require('./users/users.service.js');
+const products = require('./products/products.service.js');
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
   app.configure(stores);
   app.configure(users);
+  app.configure(products);
 };
